2ed35f8b1140b7369f5634422008190eb4061f3c
nbrosson <nbrosson@yolaw.fr>
2022-06-29 15:40:00 +0200

    remove provider version

--- providers.tf
+++ providers.tf
@@ -1,8 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = ">= 3.0.0, < 4.0.0"
+      source = "hashicorp/aws"
     }
   }
   required_version = ">= 1.0"
